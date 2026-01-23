a135241c5bfb73f2f4c2ccc634d09856fa668fe0
caoimhemorley <167096365+caoimhemorley@users.noreply.github.com>
2026-01-23 11:42:03 +0000

    tidying up

--- R/02_main_analyses/Fig5a-d_Spearman_correlation.R
+++ R/02_main_analyses/Fig5a-d_Spearman_correlation.R
@@ -18,7 +18,8 @@ format_pval <- function(p) {
 }
 
 make_correlation_plot <- function(df, x_var, y_var, group_var, x_label, y_label) {
-  df <- df %>% dplyr::filter(!is.na(.data[[x_var]]) & !is.na(.data[[y_var]]))
+  df <- df %>%
+    dplyr::filter(!is.na(.data[[x_var]]) & !is.na(.data[[y_var]]))
   if (nrow(df) < 3) stop("Not enough observations")
   
   cor_res <- stats::cor.test(df[[x_var]], df[[y_var]], method = "spearman")
@@ -33,17 +34,25 @@ make_correlation_plot <- function(df, x_var, y_var, group_var, x_label, y_label)
   y_max <- ceiling(max(df[[y_var]]))
   annot_y <- y_max - (y_max - y_min) * 0.1
   
-  groups <- unique(df[[group_var]])
-  color_mapping <- setNames(
-    c("aquamarine4", "darkorchid4", "gray70")[seq_along(groups)],
-    groups
+  df[[group_var]] <- factor(df[[group_var]], levels = c("OPCA", "SND"))
+  
+  color_mapping <- c(
+    "OPCA" = "aquamarine4",
+    "SND"  = "darkorchid4"
   )
   
-  p <- ggplot2::ggplot(df, ggplot2::aes(x = .data[[x_var]], y = .data[[y_var]], color = .data[[group_var]])) +
+  p <- ggplot2::ggplot(
+    df,
+    ggplot2::aes(
+      x = .data[[x_var]],
+      y = .data[[y_var]],
+      color = .data[[group_var]]
+    )
+  ) +
     ggplot2::geom_point(size = 5, alpha = 0.9) +
     ggplot2::geom_smooth(method = "lm", se = FALSE, color = "black")
   
-  for (g in groups) {
+  for (g in levels(df[[group_var]])) {
     df_g <- df %>% dplyr::filter(.data[[group_var]] == g)
     if (nrow(df_g) >= 2) {
       p <- p + ggplot2::geom_smooth(
@@ -73,10 +82,10 @@ make_correlation_plot <- function(df, x_var, y_var, group_var, x_label, y_label)
     ggplot2::theme_minimal() +
     ggplot2::theme(
       legend.position = "top",
-      legend.title = ggplot2::element_text(size = 16),
-      legend.text = ggplot2::element_text(size = 14),
-      axis.title = ggplot2::element_text(size = 16),
-      axis.text = ggplot2::element_text(size = 14),
+      legend.title = ggplot2::element_text(size = 14),
+      legend.text = ggplot2::element_text(size = 12),
+      axis.title = ggplot2::element_text(size = 14),
+      axis.text = ggplot2::element_text(size = 12),
       panel.border = ggplot2::element_rect(color = "black", fill = NA, linewidth = 1),
       plot.margin = ggplot2::margin(5, 15, 5, 15)
     )
@@ -93,7 +102,7 @@ msa_regions <- msa_only_data %>%
     values_from = Percentage_SOX10pos_gain
   ) %>%
   dplyr::mutate(
-    most_affected = dplyr::if_else(Classification == "OPCA", Cerebellum, Putamen),
+    most_affected  = dplyr::if_else(Classification == "OPCA", Cerebellum, Putamen),
     least_affected = dplyr::if_else(Classification == "OPCA", Putamen, Cerebellum)
   )
 
@@ -102,36 +111,32 @@ age_onset_pos_df <- avg_cnv_data_msa_only %>%
     !is.na(Age_of_onset),
     !is.na(Percentage_Average_SOX10pos_gain)
   ) %>%
-  dplyr::mutate(
-    group = dplyr::if_else(Classification == "OPCA", "OPCA Cerebellum", "SND Putamen")
-  )
+  dplyr::mutate(group = Classification)
 
 age_onset_neg_df <- avg_cnv_data_msa_only %>%
   dplyr::filter(
     !is.na(Age_of_onset),
     !is.na(Percentage_Average_SOX10neg_gain)
   ) %>%
-  dplyr::mutate(
-    group = dplyr::if_else(Classification == "OPCA", "OPCA Cerebellum", "SND Putamen")
-  )
+  dplyr::mutate(group = Classification)
 
 msa_gain_df <- msa_only_data %>%
   dplyr::filter(
     !is.na(Percentage_SOX10neg_gain),
     !is.na(Percentage_SOX10pos_gain)
   ) %>%
-  dplyr::mutate(
-    group = dplyr::if_else(Classification == "OPCA", "OPCA Cerebellum", "SND Putamen")
-  )
+  dplyr::mutate(group = Classification)
 
 plot1 <- make_correlation_plot(
   msa_regions,
   x_var = "most_affected",
   y_var = "least_affected",
   group_var = "Classification",
-  x_label = "Most affected region (% SOX10+ SNCA gains)",
-  y_label = "Least affected region (% SOX10+ SNCA gains)"
+  x_label = expression("Most affected region (% SOX10+" ~ italic(SNCA) ~ " gains)"),
+  y_label = expression("Lesser affected region (% SOX10+" ~ italic(SNCA) ~ " gains)")
 )
+
+
 
 plot2 <- make_correlation_plot(
   age_onset_pos_df,
@@ -139,17 +144,19 @@ plot2 <- make_correlation_plot(
   y_var = "Percentage_Average_SOX10pos_gain",
   group_var = "group",
   x_label = "Age of onset (years)",
-  y_label = "Average % SOX10+ SNCA gains"
+  y_label = expression("Average % SOX10+" ~ italic(SNCA) ~ " gains")
 )
+
 
 plot3 <- make_correlation_plot(
   msa_gain_df,
   x_var = "Percentage_SOX10neg_gain",
   y_var = "Percentage_SOX10pos_gain",
   group_var = "group",
-  x_label = "% SOX10- SNCA gains",
-  y_label = "% SOX10+ SNCA gains"
+  x_label = expression("% SOX10-" ~ italic(SNCA) ~ " gains"),
+  y_label = expression("% SOX10+" ~ italic(SNCA) ~ " gains")
 )
+
 
 plot4 <- make_correlation_plot(
   age_onset_neg_df,
@@ -157,48 +164,52 @@ plot4 <- make_correlation_plot(
   y_var = "Percentage_Average_SOX10neg_gain",
   group_var = "group",
   x_label = "Age of onset (years)",
-  y_label = "Average % SOX10- SNCA gains"
+  y_label = expression("Average % SOX10-" ~ italic(SNCA) ~ " gains")
 )
+
 
 combined_plot <- (plot1 + plot2) / (plot3 + plot4)
 
 cor_plot_dir <- plots_spearman_dir
 
+
+cor_plot_dir <- plots_spearman_dir
+
 ggplot2::ggsave(
-  filename = file.path(cor_plot_dir, "FISH_Spearman_Correlation_Most_vs_Least_Affected_SOX10pos_SNCA_gain.png"),
-  plot = plot1,
+  file.path(cor_plot_dir, "FISH_Spearman_Correlation_Most_vs_Least_Affected_SOX10pos_SNCA_gain.png"),
+  plot1,
   width = 6,
   height = 6,
   dpi = 600
 )
 
 ggplot2::ggsave(
-  filename = file.path(cor_plot_dir, "FISH_Spearman_Correlation_Avg_SOX10pos_SNCA_gain_vs_Age_of_onset.png"),
-  plot = plot2,
+  file.path(cor_plot_dir, "FISH_Spearman_Correlation_Avg_SOX10pos_SNCA_gain_vs_Age_of_onset.png"),
+  plot2,
   width = 6,
   height = 6,
   dpi = 600
 )
 
 ggplot2::ggsave(
-  filename = file.path(cor_plot_dir, "FISH_Spearman_Correlation_SOX10neg_SNCA_gain_vs_SOX10pos_SNCA_gain.png"),
-  plot = plot3,
+  file.path(cor_plot_dir, "FISH_Spearman_Correlation_SOX10neg_vs_SOX10pos_SNCA_gain.png"),
+  plot3,
   width = 6,
   height = 6,
   dpi = 600
 )
 
 ggplot2::ggsave(
-  filename = file.path(cor_plot_dir, "FISH_Spearman_Correlation_Avg_SOX10neg_SNCA_gain_vs_Age_of_onset.png"),
-  plot = plot4,
+  file.path(cor_plot_dir, "FISH_Spearman_Correlation_Avg_SOX10neg_SNCA_gain_vs_Age_of_onset.png"),
+  plot4,
   width = 6,
   height = 6,
   dpi = 600
 )
 
 ggplot2::ggsave(
-  filename = file.path(cor_plot_dir, "FISH_Spearman_Correlation_SOX10_SNCA_gain_Combined_2x2.png"),
-  plot = combined_plot,
+  file.path(cor_plot_dir, "FISH_Spearman_Correlation_SOX10_SNCA_gain_Combined_2x2.png"),
+  combined_plot,
   width = 12,
   height = 12,
   dpi = 600
